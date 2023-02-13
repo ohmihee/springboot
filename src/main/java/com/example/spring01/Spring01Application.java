b2627ec5ccff400c5b98c8fd171c70fdecd9f3be
mihee <algml9603@gmail.com>
2023-02-13 20:16:06 +0900

    feat: init

---
+++ src/main/java/com/example/spring01/Spring01Application.java
@@ -0,0 +1,13 @@
+package com.example.spring01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring01Application {
+    public static void main(String[] args) {
+        System.out.println("test==");
+        SpringApplication.run(Spring01Application.class, args);
+    }
+
+}
